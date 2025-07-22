ad6656fdd9e36baba42b9ade4c89fdd8ff83b050
Erick Kamanda <info.proresearchers@gmail.com>
2025-07-22 10:55:29 +0300

    Simple learning_log web app

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+  """A topic that the user is learning about"""
+  text = models.CharField(max_length=255)
+  date_added = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+    """Return a string representation of the model"""
+    return self.text
+  
+class Entry(models.Model):
+  """Something a user learns about a topic"""
+  topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+  text = models.TextField()
+  date_added = models.DateField(auto_now_add=True)
+
+  class Meta:
+    """Meta options for Entry model"""
+    verbose_name = 'entry'
+    verbose_name_plural = 'Entries'
+
+  def __str__(self):
+    """Return a string representation of themodel"""
+    return f"{self.text[:50]}..." if len(self.text) > 50 else self.text
---
+++ learning_logs/urls.py
@@ -0,0 +1,15 @@
+"""Defines URL patterns for learning_logs app."""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+  #Home Page
+  path('', views.index, name='index'),
+  #Page that shows all topics
+  path('topics/', views.topics, name='topics'),
+  #Detail page for a single topic
+  path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
