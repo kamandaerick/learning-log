7da335fa388e09231a0c60ec31250dff0c71c3de
Erick Kamanda <info.proresearchers@gmail.com>
2025-08-01 21:03:59 +0300

    Create a 404.html file in the outermost project forlder

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
   """A topic that the user is learning about"""
   text = models.CharField(max_length=255)
+  owner = models.ForeignKey(User, on_delete=models.CASCADE)
   date_added = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
