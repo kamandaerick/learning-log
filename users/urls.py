0b3870e6da6b607f007965afd8ebc6f7dbc222df
Erick Kamanda <info.proresearchers@gmail.com>
2025-08-01 13:02:36 +0300

    Create a User Registration view

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,15 @@
 """Define url patterns for user management"""
 
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
     # Include the default auth urls
     path('', include('django.contrib.auth.urls')),
+    # Registration page
+    path('register/', views.register, name='register'),
 ]
 
 
