934b8972040a1ace9cbd8bfc77880fbb30766a70
Erick Kamanda <info.proresearchers@gmail.com>
2025-08-02 11:22:25 +0300

    restrict users to only see the topics they have added

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -11,14 +12,14 @@ def index(request):
 @login_required
 def topics(request):
   '''Show all topics'''
-  topics = Topic.objects.order_by('date_added')
+  topics = Topic.objects.filter(owner=request.user).order_by('date_added')
   context = {'topics': topics}
   return render(request, 'learning_logs/topics.html', context)
 
 @login_required
 def topic(request, topic_id):
   '''Show a single topic and all its entries'''
-  topic = Topic.objects.get(id=topic_id)
+  topic = get_object_or_404(Topic, id=topic_id)
   entries = topic.entry_set.order_by('-date_added')
   context = {'topic': topic, 'entries': entries}
   return render(request, 'learning_logs/topic.html', context)
